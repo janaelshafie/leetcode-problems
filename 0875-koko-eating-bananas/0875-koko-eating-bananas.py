e2c45b13c7395fedd59c3d666951c3449ba52b7b
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-08-31 18:01:27 +0300

    Time: 149 ms (92.67%), Space: 13.6 MB (15.2%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,29 @@
+import math
+class Solution(object):
+    def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+            totalHours = 0
+
+            for pile in piles:
+                totalHours += (pile + k - 1) // k
+
+            if totalHours <= h:
+                res = k
+                r = k - 1
+
+            else:
+                l = k + 1
+
+        return res
+
+
+        
