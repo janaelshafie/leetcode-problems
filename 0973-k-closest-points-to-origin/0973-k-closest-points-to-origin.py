bb8807e64597f0232c9bb4f054b59cbfd7c43629
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-09 17:42:42 +0300

    Time: 76 ms (27.99%), Space: 24.5 MB (70.82%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,39 +1,30 @@
-class Solution(object):
-    def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        self.quickSelect(points,k - 1,0,len(points) - 1)
-        return points[:k]
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        res = []
+        i = 0
+        while len(heap) < k:
+            x = (points[i][0] ** 2) + (points[i][1] ** 2)
+            heap.append((x,points[i]))
+            i += 1
 
-    def quickSelect(self, points, k, s, e):
-        left = s
-        pivot_index = random.randint(s,e)
-        points[pivot_index], points[e] = points[e], points[pivot_index]
-        pivot = points[e]
-        res_pivot = (points[e][0] - 0) ** 2 + (points[e][1] - 0) ** 2
-        for i in range(s, e):
-            if (points[i][0] - 0) ** 2 + (points[i][1] - 0) ** 2 <= res_pivot:
-                temp = points[left]
-                points[left] = points[i]
-                points[i] = temp
-                left += 1
+        heapq.heapify_max(heap)
 
-        points[e] = points[left]
-        points[left] = pivot
+        while i < len(points):
+            val, point = heap[0]
+            x = (points[i][0] ** 2) + (points[i][1] ** 2)
+            if x < val:
+                heapq.heappushpop_max(heap,(x,points[i]))
+            i += 1
 
-        if k == left:
-            return points
+        while k > 0:
+            val, point = heapq.heappop_max(heap)
+            res.append(point)
+            k -= 1
 
-        elif k < left:
-            return self.quickSelect(points,k,s,left - 1)
+        return res
 
-        else:
-            return self.quickSelect(points,k,left + 1,e)
         
-
-
 
         
