d383c0388a84a8ed5a77869b0d3f913d067312d7
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-09 17:59:06 +0300

    Time: 110 ms (25.68%), Space: 29.5 MB (97.37%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,43 +1,10 @@
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
-        self.mergeSort(nums, 0, len(nums) - 1)
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify_max(nums)
 
-        return nums[k - 1]
+        while k > 1:
+            heapq.heappop_max(nums)
+            k -= 1
 
-    def mergeSort(self, nums, s, e):
-
-        if s >= e:
-            return
-
-        m = (s + e) // 2
-
-        self.mergeSort(nums, s, m)
-        self.mergeSort(nums, m + 1, e)
-        self.merge(nums, s, m, e)
-
-        return nums
-
-    def merge(self, nums, s, m, e):
-        temp = []
-        i = s
-        j = m + 1
-        while i <= m and j <= e:
-            if nums[i] >= nums[j]:
-                temp.append(nums[i])
-                i += 1
-            else:
-                temp.append(nums[j])
-                j += 1
-
-        temp.extend(nums[j: e + 1])
-        temp.extend(nums[i:m + 1])
-
-        nums[s: e + 1] = temp
-        return nums
+        return nums[0]
         
