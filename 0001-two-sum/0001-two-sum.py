1d3bbc60f76a2c63b2376e40fa5055ebfa18714b
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-10 16:38:31 +0300

    Time: 3 ms (54.68%), Space: 13.4 MB (17.09%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hm = {}
+        for i in range(len(nums)):
+            x = target - nums[i]
+            if x in hm:
+                return [hm[x], i]
+            hm[target - x] = i
+            
+
+        
