af94fa2c7b4fd6b2fa283dc3dd61db092e90382d
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-12 01:02:47 +0300

    Time: 268 ms (19.1%), Space: 31 MB (10.88%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        icount = 0
+        ROWS,COLS = len(grid), len(grid[0])
+        self.visit = set()
+        def dfs(r,c):
+            if (min(r,c) < 0 or r == ROWS or c == COLS or
+                grid[r][c] == "0" or (r,c) in self.visit):
+                return
+
+            self.visit.add((r,c))
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in self.visit:
+                    dfs(r, c)
+                    icount += 1
+
+        return icount
+            
+
+            
+
+            
+
+
+        
