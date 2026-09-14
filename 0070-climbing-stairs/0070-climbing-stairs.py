ce6a6b34cf8a05738bf4c24f05a376be8a3e2413
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-14 17:19:53 +0300

    Time: 0 ms (100%), Space: 19.2 MB (86.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        cache = {}
+        def dp(n):
+            if n <= 2:
+                return n
+
+            if n in cache:
+                return cache[n]
+
+
+            cache[n] = dp(n - 1) + dp(n - 2)
+
+            return cache[n]
+
+        return dp(n)
+        
