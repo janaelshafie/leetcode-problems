5815abd867cd57e77c4fa6f211c4a4cd88178ec6
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-14 18:25:40 +0300

    Time: 0 ms (100%), Space: 19.4 MB (24.91%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        self.opt_1 = 0
+        self.opt_2 = 0
+        cache = {}
+        if not nums:
+            return 0
+
+        if len(nums) == 1:
+            return nums[0]
+
+        if len(nums) == 2:
+            return max(nums[0],nums[1])
+
+        def dp(i):            
+            if i > len(nums) - 1:
+                return 0
+
+            if i in cache:
+                return cache[i]
+
+            opt1 = nums[i] + dp(i + 2)
+            opt2 = dp(i + 1)
+
+            cache[i] = max(opt1, opt2)
+
+            return cache[i]
+
+        return dp(0)
+        
