bc2ce1e1f5c2097328cefb66e38074f951d6a3a6
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-15 17:46:55 +0300

    Time: 1 ms (28.69%), Space: 19.2 MB (75.34%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        def memo(r, c, cache):
+            if r >= m or c >= n:
+                return 0
+
+            if r == m - 1 and c == n - 1:
+                return 1
+
+            if cache[r][c] > 0:
+                return cache[r][c]
+
+            cache[r][c] = memo(r + 1, c, cache) + memo(r, c + 1, cache)
+
+            return cache[r][c]
+
+        return memo(0, 0, [[0] * n for _ in range(m)])
+
+        
