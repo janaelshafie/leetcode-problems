ef1f688d2a8d8b93ca05018e00aa97b2ad87c1ac
Jana Hany Elshafie <168121874+janaelshafie@users.noreply.github.com>
2026-09-20 19:30:32 +0300

    Time: 17 ms (23.81%), Space: 24 MB (17.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,29 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
         hmap = {}
+        # for s in strs:
+        #     key = "".join(sorted(s))
+
+        #     if key not in hmap:
+        #         hmap[key] = []
+    
+        #     hmap[key].append(s)
+
+        # return list(hmap.values())
+
+        #or instead of sorting we could do a frequency counting
+
         for s in strs:
-            key = "".join(sorted(s))
+            count = [0] * 26
+
+            for char in s:
+                count[ord(char) - ord("a")] += 1
+
+            key = tuple(count)
 
             if key not in hmap:
                 hmap[key] = []
-    
+
             hmap[key].append(s)
 
         return list(hmap.values())
